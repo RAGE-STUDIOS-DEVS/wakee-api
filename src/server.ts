d800603f25f1e5ad4c3dfb2b782d269c73d9e52b
yosukeyoh <yosukeonrails@gmail.com>
2023-11-22 21:31:35 -0800

    [#001] added dot env

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,20 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 const app = express();
 app.use(express.json());
-const PORT = 8080;
+
+
+const port = process.env.NODE_ENV === 'local' ? 8080 : process.env.URL;
 
 app.get('/', (req, res, next) => {
 	res.status(200).json({
 		'message': 'Running Node with Express and Typescript'
 	});
 });
-app.listen(PORT, () => {
+app.listen(port, () => {
 	console.log(
-		`Server running on ${PORT}.`
+		`Server running on ${port}.`
 	)
 });
