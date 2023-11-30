1881771bf596b01f9f2f322ae95280cd42886af1
yosukeyoh <yosukeonrails@gmail.com>
2023-11-29 21:25:34 -0800

    fix port

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,7 @@ const start = async () => {
 		await mongoose.connect(
 			MONGODB_URI
 		);
-		app.listen(3000, () => console.log('Server started on port 3000'));
+		app.listen(port, () => console.log('Server started on port' + port));
 	} catch (error) {
 		console.log(error);
 		process.exit();
